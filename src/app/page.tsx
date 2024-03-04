9c7f82119d10321a86d543d110f4f1ecdc27e28c
Vinicius de Oliveira <33381159+vinic2000@users.noreply.github.com>
2024-03-04 10:38:09 -0300

    bugfix: teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By TEste
+            By TEste -
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
